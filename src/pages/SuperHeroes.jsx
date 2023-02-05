955ce053665e9dd3fadbf3475c667782cdb9a05c
azamatyudaev <azamatyudaev@gmail.com>
2023-02-05 22:20:41 +0500

    Handling error traditional way

--- src/pages/SuperHeroes.jsx
+++ src/pages/SuperHeroes.jsx
@@ -4,17 +4,24 @@ import { useEffect, useState } from 'react'
 const SuperHeroes = () => {
   const [loading, setLoading] = useState(true)
   const [data, setData] = useState([])
-  console.log(data)
+  const [error, setError] = useState('')
 
   useEffect(() => {
     axios.get('http://localhost:4000/superheroes').then((response) => {
       setData(response.data)
+      setLoading(false)
+    }).catch(error => {
+      setError(error.message)
       setLoading(false)
     })
   }, [])
 
   if (loading) {
     return <h2>Loading...</h2>
+  }
+
+  if (error) {
+    return <h2>{error}</h2>
   }
 
   return (
